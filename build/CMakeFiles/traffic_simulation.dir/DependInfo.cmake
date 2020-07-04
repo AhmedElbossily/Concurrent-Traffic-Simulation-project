0d34e1d1040d5bc2062d1ef90ff509519ecb21be
Elbossily <ahmed.fathy.elbossily@gmail.com>
2020-07-04 13:28:19 +0200

    second

---
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/src/Graphics.cpp" "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/src/Intersection.cpp" "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/src/Street.cpp" "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/src/TrafficLight.cpp" "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.o"
+  "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/src/TrafficObject.cpp" "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/src/TrafficSimulator-Final.cpp" "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.o"
+  "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/src/Vehicle.cpp" "/media/elbossily/New Volume/cplusplus/Udacity/Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/TrafficLight.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <random>
+#include "TrafficLight.h"
+#include <future>
+
+/* Implementation of class "MessageQueue" */
+
+template <typename T>
+T MessageQueue<T>::receive()
+{
+	// FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait()
+	// to wait for and receive new messages and pull them from the queue using move semantics.
+	// The received object should then be returned by the receive function.
+
+	/* Create a lock and pass it to the condition variable */
+	std::unique_lock<std::mutex> u_lock(mtx_);
+	_condition.wait(u_lock, [this] { return !_queue.empty(); });
+
+	/* Get the latest element and remove it from the queue */
+	T msg = std::move(_queue.back());
+	_queue.pop_back();
+	//_queue.clear();
+	return msg;
+}
+
+template <typename T>
+void MessageQueue<T>::send(T &&msg)
+{
+	// .4a : The method send should use the mechanisms std::lock_guard<std::mutex>
+	// as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+	/* Prevent data race */
+	std::lock_guard<std::mutex> u_lock(mtx_);
+
+	/* Move into queue and notify client */
+	if (!_queue.empty()) _queue.clear();
+
+	_queue.push_back(std::move(msg));
+	_condition.notify_one();
+}
+
+/* Implementation of class "TrafficLight" */
+
+TrafficLight::TrafficLight()
+{
+	_currentPhase = TrafficLightPhase::red;
+
+	//iniitiate shared pointer /*****************************************************************************************************/
+	msg_queue_ = std::make_shared<MessageQueue<TrafficLightPhase>>();
+}
+
+void TrafficLight::waitForGreen()
+{
+	// FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop
+	// runs and repeatedly calls the receive function on the message queue.
+	// Once it receives TrafficLightPhase::green, the method returns.
+	while (true)
+	{
+		/* Sleep at every iteration to reduce CPU usage */
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+		/* Wait until the traffic light is green, received from message queue */
+		auto curr_phase = msg_queue_->receive();
+		if (curr_phase == green)
+		{
+			return;
+		}
+	}
+}
+
+TrafficLightPhase TrafficLight::getCurrentPhase()
+{
+	return _currentPhase;
+}
+
+void TrafficLight::simulate()
+{
+	// FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class.
+	threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
+}
+
+// virtual function which is executed in a thread
+void TrafficLight::cycleThroughPhases()
+{
+	// FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles
+	// and toggles the current phase of the traffic light between red and green and sends an update method
+	// to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds.
+	// Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
+
+	/* Init our random generation between 4 and 6 seconds */
+	std::random_device rd;
+	std::mt19937 eng(rd());
+	std::uniform_int_distribution<> distr(4, 6);
+
+	/* Print id of the current thread */
+	std::unique_lock<std::mutex> lck(mtx_);
+	std::cout << "Traffic_Light #" << _id << "::Cycle_Through_Phases: thread id = " << std::this_thread::get_id() << std::endl;
+	lck.unlock();
+
+	/* Initalize variables */
+	int cycle_duration = distr(eng); //Duration of a single simulation cycle in seconds, is randomly chosen
+
+	/* Init stop watch */
+	auto last_update = std::chrono::system_clock::now();
+	while (true)
+	{
+		/* Compute time difference to stop watch */
+		long time_since_last_update = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - last_update).count();
+
+		/* Sleep at every iteration to reduce CPU usage */
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+		/* It is time to toggle our traffic light */
+		if (time_since_last_update >= cycle_duration)
+		{
+			/* Toggle current phase of traffic light */
+			if (_currentPhase == red)
+			{
+				_currentPhase = green;
+			}
+			else
+			{
+				_currentPhase = red;
+			}
+
+			/* Send an update to the message queue and wait for it to be sent */
+			auto msg = _currentPhase;
+			auto is_sent = std::async(std::launch::async, &MessageQueue<TrafficLightPhase>::send, msg_queue_, std::move(msg));
+			is_sent.wait();
+
+			/* Reset stop watch for next cycle */
+			last_update = std::chrono::system_clock::now();
+
+			/* Randomly choose the cycle duration for the next cycle */
+			cycle_duration = distr(eng);
+		}
+	}
+}
